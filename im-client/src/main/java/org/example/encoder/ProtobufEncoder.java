bacb3a478d952532fcccf7a94b13c6704f0c8d38
liumincong <liumincongfs@gmail.com>
2025-12-25 20:16:29 +0800

    commit

--- im-client/src/main/java/org/example/encoder/ProtobufEncoder.java
+++ im-client/src/main/java/org/example/encoder/ProtobufEncoder.java
@@ -3,10 +3,14 @@ package org.example.encoder;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToByteEncoder;
+import org.example.protos.MsgProtos;
 
-public class ProtobufEncoder extends MessageToByteEncoder {
+public class ProtobufEncoder extends MessageToByteEncoder<MsgProtos.Msg> {
     @Override
-    protected void encode(ChannelHandlerContext channelHandlerContext, Object o, ByteBuf byteBuf) throws Exception {
-
+    protected void encode(ChannelHandlerContext channelHandlerContext, MsgProtos.Msg msg, ByteBuf byteBuf) throws Exception {
+        byte[] bytes = msg.toByteArray();
+        int length = bytes.length;
+        byteBuf.writeShort(length);
+        byteBuf.writeBytes(msg.toByteArray());
     }
 }
